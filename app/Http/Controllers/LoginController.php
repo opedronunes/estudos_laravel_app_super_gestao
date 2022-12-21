328a123061df2cffb56240b570960f5dda080152
Pedro Nunes <adm.pedronunes@hotmail.com>
2022-12-21 20:23:31 -0300

    modified Logincontroller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -49,6 +49,7 @@ class LoginController extends Controller
             ->where('password', $password)
             ->get()
             ->first();
+            
 
         if (isset($usuario->name)) {
             
